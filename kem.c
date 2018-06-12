98cf8f5e0a0cc533579abb39f34c2fadf6d4fcbb
kinnian <julia.callebat@telecom-paristech.fr>
2018-06-12 14:57:46 +0200

    Correction des erreurs d'exécution L'exécution termine, mais renvoie un mauvais résultat ; à corriger.

--- kem.c
+++ kem.c
@@ -9,27 +9,46 @@
 // Fonctions annexes
 // Cas ou la liste est une representation binaire
 int char_to_int(unsigned char* c, int n_) {
+//	printf("char to int...\n");
+
 	int a = 0;
 	for (int i = 0; i < n_; i++) {
 		int j = c[i] - '0';
 		a = a + (int)(pow(2, (double)i))*j;
 	}
+
+//	printf("...cti fin\n");
 	return a;
 }
 
 // Cas ou la liste est une liste d'octets
 int char_to_int_bytes(unsigned char * c, int size) {
+//	printf("char(b) to int...\n");
+
 	int a = 0;
 	for (int i = 0; i < size; i++) {
-		int j = atoi((char *)&c[i]);
+		int j = c[i] - '0';
+//		printf("j = %i\n", j);
 		a = a + (int)(pow(2, (double)(i*8)))*j;
 	}
+
+//	printf("...ctib fin\n");
 	return a;
 }
 
-void int_to_char(int a, unsigned char c[K]) {
-	for (int i = 0; i < K; i ++) {
+// Renvoie une liste en binaire
+void int_to_char(int a, unsigned char c[n]) {
+	for (int i = 0; i < n; i ++) {
 		c[i] = (unsigned char)(a % (int)(pow(2, i)));
+	}
+	c[K] = '\0';
+	return;
+}
+
+// Renvoie une liste en octets
+void int_to_char_bytes(int a, unsigned char c[K]) {
+	for (int i = 0; i < K; i ++) {
+		c[i] = (unsigned char)(a % (int)(pow(2, 8*i)));
 	}
 	c[K] = '\0';
 	return;
@@ -44,23 +63,28 @@ int random_mod(int m, int seed) {
         return v;
 }
 
-void generate_h_sparse_string(int m, unsigned char B[K], int seed) {
-	memset(B, 0, K);
+// Genere une liste en binaire
+void generate_h_sparse_string(int m, unsigned char B[n], int seed) {
+//	printf("Generate h sparse...\n");
+	memset(B, 0, n);
 	memset(B, 1, (unsigned long)m);
 	int i = m -1;
 	int j;
 	while (i >= 0) {
-		j = random_mod(K - i, seed);
+		seed = rand(); //TODO: trouver comment rendre random_mod(x, seed) indep. de random_mod(y, seed)
+		j = random_mod(n - i, seed);
 		unsigned char a = B[i];
-		printf("indice i+j : %i, taille K : %i\n", i+j, m);
+//		printf("i = %i, indice i+j : %i, taille K : %i\n", i, i+j, K);
 		B[i] = B[i+j];
 		B[i+j] = a;
 		i--;
 	}	
 	
+//	printf("...ghs fin\n");
 	return;
 }
 
+// Necessite une liste en binaire
 int h_weight(unsigned char * B) {
 	int w= 0;
 	int size = sizeof(B) / sizeof(B[0]);
@@ -82,8 +106,9 @@ void get_subarray(unsigned char * A, unsigned char * B, int first, int last) {
 	return;
 }
 
-void xor(unsigned char A[K], unsigned char B[K], unsigned char C[K]) {
-	for (int i = 0; i < K; i ++) {
+// Necessite une liste en binaire
+void xor(unsigned char A[n], unsigned char B[n], unsigned char C[n]) {
+	for (int i = 0; i < n; i ++) {
 		if (A[i] == B[i]) {
 			C[i] = 0;
 		}
@@ -97,14 +122,17 @@ void xor(unsigned char A[K], unsigned char B[K], unsigned char C[K]) {
 
 // Generation de cles
 void det_key_pair(int * sk, unsigned char * pk, int seed){
-	// Generation de deux arrays de poids h, de taille n
-	unsigned char A_f[K];
+//	printf("Det key pair...\n");
+	// Generation de deux listes de poids h, de taille n
+	// Ici, listes en bits
+	unsigned char A_f[n];
 	generate_h_sparse_string(h, A_f, seed);
-	unsigned char A_g[K];
+	unsigned char A_g[n];
 	generate_h_sparse_string(h, A_g, seed);
 	
 
-	// Generation d'un array de K octets
+	// Generation d'un liste de K octets
+	// Ici, liste en octets
 	unsigned char A_R[K];
 	for (int i = 0; i < K; i ++) { 
 		A_R[i] = (unsigned char)(rand());
@@ -114,21 +142,26 @@ void det_key_pair(int * sk, unsigned char * pk, int seed){
 	int size = sizeof(A_f) / sizeof(A_f[0]);
 	f = char_to_int(A_f, size);
 	g = char_to_int(A_g, size);
+
 	size = sizeof(A_R) / sizeof(A_R[0]);
-	R = (char_to_int(A_R, size)) % P;
+	R = (char_to_int_bytes(A_R, size)) % P;
 	
 	T = (f*R + g) % P;
 	unsigned char A_T[K];
-	int_to_char(T, A_T);
+	int_to_char_bytes(T, A_T);
 
 	strcpy((char *)pk, (char *)A_R);
 	strcat((char *)pk, (char *)A_T);
 	sk = &f;
 
+	printf("sk = %i\n", *sk);
+	printf("pk = %s\n", pk);
+//	printf("...dkp fin\n");
 	return;
 }
 
 void key_pair(int * sk, unsigned char * pk) {
+//	printf("Key pair...\n");
 
 	// Generation d'un array de 32 octets
 	unsigned char SK[32];
@@ -144,15 +177,18 @@ void key_pair(int * sk, unsigned char * pk) {
 	// sk doit etre SK en int ; c'est exactement seed.
 	sk = &seed;
 	
+//	printf("sk = %i\n", *sk);
+//	printf("pk : %s\n", pk);
+//	printf("...kp fin\n");
 	return;
 }
 
 
 // Encapsulation d'un secret commun SS
-void det_kem_enc(unsigned char *pk, unsigned char * C, unsigned char * SS, unsigned char * S){
-	// On traduit l'array graine en entier
+void det_kem_enc(unsigned char *pk, unsigned char * C, unsigned char SS[32], unsigned char * S){
+	// On traduit la liste graine en entier
 	int size = (int) sizeof(S) / sizeof(S[0]);
-	int seed = char_to_int(S, size);
+	int seed = char_to_int_bytes(S, size);
 
 	// On rempli l'array SS au hasard
 	for (int i = 0; i < 32; i ++) { 
@@ -160,7 +196,8 @@ void det_kem_enc(unsigned char *pk, unsigned char * C, unsigned char * SS, unsig
 	}	
 	
 	// Generation de a, b1 et b2 pseudo aleatoire de poids h
-	unsigned char A_a[K], A_b1[K], A_b2[K];
+	// generate_h_sparse_string renvoie des listes de bits
+	unsigned char A_a[n], A_b1[n], A_b2[n];
 	generate_h_sparse_string(h, A_a, seed);
 	generate_h_sparse_string(h, A_b1, seed);
 	generate_h_sparse_string(h, A_b2, seed);
@@ -176,9 +213,9 @@ void det_kem_enc(unsigned char *pk, unsigned char * C, unsigned char * SS, unsig
 	get_subarray(pk, A_R, 0, K);
 	get_subarray(pk, A_T, K + 1, 2*K); 
 	size = (int)sizeof(A_R) / sizeof(A_R[0]);
-	r = char_to_int(A_R, size);
+	r = char_to_int_bytes(A_R, size);
 	size = (int)sizeof(A_T) / sizeof(A_T[0]);
-	t = char_to_int(A_T, size);
+	t = char_to_int_bytes(A_T, size);
 
 	// On calcule le chiffre
 	int c1, c2;
@@ -190,30 +227,42 @@ void det_kem_enc(unsigned char *pk, unsigned char * C, unsigned char * SS, unsig
 	M = (unsigned char*) calloc(32*rho, sizeof(char));
 	for (int i = 0; i < 255; i ++) {
 		if (S[i] == 0) { 
-			for (int j = i*rho/8; j < (j+1)*rho/8 - 1; j ++) {
+			for (int j = i*rho/8; j < (i+1)*rho/8 - 1; j ++) {
 				M[j] = 0;
 			}
 		}
 		else {
-			for (int j = i*rho/8; j < (j+1)*rho/8 - 1; j ++) {
+			for (int j = i*rho/8; j < (i+1)*rho/8 - 1; j ++) {
 				M[j] = 255;
 			}
 		}
 	}
 
-	// On enregistre le chiffre sous forme d'arrays
+	// On enregistre le chiffre sous forme de listes d'octets
 	unsigned char C1[K], C2[K];
-       	int_to_char(c1, C1);
-	int_to_char(c2, C2);
-	xor(M, C2, C2);
+       	int_to_char_bytes(c1, C1);
+	
+	// xor se fait sur des listes de bits, il faut effectuer la conversion
+	unsigned char MM[n], CC2[n];
+	int mm;
+	int_to_char(c2, CC2);
+	size = (int)sizeof(M) / sizeof(M[0]);
+	mm = char_to_int_bytes(M, size);
+	int_to_char_bytes(mm, MM);
+	xor(MM, CC2, CC2);
+
+	//on re-convertis C2 en liste d'octets
+	size = (int)sizeof(CC2) / sizeof(CC2[0]);
+	c2 = char_to_int(CC2, size);
+	int_to_char_bytes(c2, C2);
 	strcpy((char *)C, (char *)C1);
 	strcat((char *)C, (char *)C2);
 
 	return;
 }
 
-void kem_enc(unsigned char * pk, unsigned char * CT, unsigned char * SS) {
-	// On genere la graine sous forme d'array d'octets
+void kem_enc(unsigned char * pk, unsigned char * CT, unsigned char SS[32]) {
+	// On genere la graine sous forme de liste d'octets
 	unsigned char S[32];
 	for (int i = 0; i < 32; i ++) {
 		S[i] = (unsigned char)(rand());
@@ -236,21 +285,34 @@ int kem_dec(int * sk, unsigned char * C, unsigned char * SS){
 	get_subarray(C, C2, K, K + 32*rho - 1);
 	int c1;
 	int size = (int)sizeof(C1) / sizeof(C1[0]);
-	c1 = char_to_int(C1, size);
+	c1 = char_to_int_bytes(C1, size);
 
 	// Calcul de PK
 	unsigned char pk[2*K];
-	int * f = 0; 
-	det_key_pair(f, pk, *sk);
+	int f; 
+	det_key_pair(&f, pk, *sk);
 
-	// Calcul de C2'
-	int c2_ = ((*f)*c1) % P;
-	unsigned char C2_[K];
+	// Calcul de C2' (en bits)
+	int c2_ = (f*c1) % P;
+	unsigned char C2_[n];
 	int_to_char(c2_, C2_);
 
 	// Calcul de M
-	unsigned char * M = 0;
-	xor(C2_, C2, M);
+	unsigned char MM[n];
+	// on transforme C2 en liste de bits
+	int c2;
+	size = (int) sizeof(C2) / sizeof(C2[0]);
+	c2 = char_to_int_bytes(C2, size);
+	unsigned char C2_bits[n];
+	int_to_char(c2, C2_bits);
+	xor(C2_, C2_bits, MM);
+
+	// on convertit MM en liste d'octets pour la suite
+	unsigned char M[K];
+	int m;
+	size = (int) sizeof(MM) / sizeof(MM[0]);
+	m = char_to_int(MM, size);
+	int_to_char_bytes(m, M);
 	
 	// On produit S'
 	unsigned char M_part[rho/8];
@@ -280,21 +342,20 @@ int kem_dec(int * sk, unsigned char * C, unsigned char * SS){
 
 // Test
 int main() {
-
-	printf("DEBUT...\n");
 	unsigned char pk[2*K];
-       	int * sk = 0;
-	key_pair(sk, pk);
+       	int sk;
+	key_pair(&sk, pk);
 
-	printf("pk : %s, sk : %c\n", pk, *sk);
+	printf("pk : %s\n", pk);
+	printf("sk : %i\n", sk);
 
 	unsigned char C[K + 32*rho];
-	unsigned char * SS = 0;
+	unsigned char SS[32];
 	kem_enc(pk, C, SS);
 
 
-	unsigned char * SS_ = 0;
-	kem_dec(sk, C, SS_);
+	unsigned char SS_[32];
+	kem_dec(&sk, C, SS_);
 
 	int ss, ss_;
 	int size = (int)sizeof(SS) / sizeof(SS[0]);
@@ -307,6 +368,5 @@ int main() {
 		printf("Echec\n");
 	}
 
-	printf("...FIN");
 	return 0;
 }
